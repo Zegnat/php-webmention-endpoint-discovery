8453f635223e63e751155f35195811ead11efea3
Martijn van der Ven <martijn@vanderven.se>
2018-09-30 12:22:41 +0200

    Introduce option to check IPv6 records

--- src/DNSResolver.php
+++ src/DNSResolver.php
@@ -8,11 +8,22 @@ use Psr\SimpleCache\CacheInterface;
 
 class DNSResolver implements DNSResolverInterface
 {
-    private $dnsCache;
+    public const INCLUDE_IPV6 = 0b0001;
+    public const PREFER_IPV6 = 0b0011;
 
-    public function __construct(CacheInterface $dnsCache)
+    private $dnsCache;
+    private $records = \DNS_A;
+    private $ipv6 = false;
+
+    public function __construct(CacheInterface $dnsCache, int $options = 0b0000)
     {
         $this->dnsCache = $dnsCache;
+        if ($options & self::INCLUDE_IPV6) {
+            $this->records += \DNS_AAAA;
+        }
+        if ($options & self::PREFER_IPV6) {
+            $this->ipv6 = true;
+        }
     }
 
     public function resolve(string $host): ?string
@@ -20,10 +31,22 @@ class DNSResolver implements DNSResolverInterface
         if (null !== $cache = $this->dnsCache->get($host)) {
             return $cache;
         }
-        $records = \dns_get_record($host, DNS_A + DNS_AAAA);
-        $first = \array_pop($records);
-        $ip = $first[$first['type'] === 'A' ? 'ip' : 'ipv6'];
-        $this->dnsCache->set($host, $ip, $first['ttl']);
+        $records = \dns_get_record($host, $this->records);
+        if (empty($records)) {
+            return null;
+        }
+        $resolved = $records[0];
+        if ('AAAA' !== $resolved['type'] && $this->ipv6) {
+            foreach ($records as $record) {
+                if ('AAAA' === $record['type']) {
+                    $resolved = $record;
+                    break;
+                }
+            }
+        }
+        $ip = $resolved['A' === $resolved['type'] ? 'ip' : 'ipv6'];
+        $this->dnsCache->set($host, $ip, $resolved['ttl']);
+
         return $ip;
     }
 }
